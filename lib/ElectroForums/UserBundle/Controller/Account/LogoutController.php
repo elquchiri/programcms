8aef4c24fba759d0326a5a14f1a88b6008816add
Mohamed <elquchiri@gmail.com>
2023-04-08 02:33:44 +0200

    Init new bundles, copyriting and code clean

--- lib/ElectroForums/UserBundle/Controller/Account/LogoutController.php
+++ lib/ElectroForums/UserBundle/Controller/Account/LogoutController.php
@@ -1,5 +1,10 @@
 <?php
-
+/*
+ * Copyright © ElectroForums. All rights reserved.
+ * See COPYING.txt for license details.
+ *
+ * Developed by Mohamed EL QUCHIRI <elquchiri@gmail.com>
+ */
 
 namespace ElectroForums\UserBundle\Controller\Account;
 
